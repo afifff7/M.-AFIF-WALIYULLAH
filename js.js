7d6d686dc0ba8ff9eea4b688877de949083f5a5f
acee <afifchebaret89@gmail.com>
2024-11-28 11:38:47 +0700

    Add files via upload

---
+++ js.js
@@ -0,0 +1,6 @@
+// JavaScript for form submission
+document.getElementById("contactForm").addEventListener("submit", function (e) {
+  e.preventDefault();
+  alert("Thank you for reaching out! I will get back to you soon.");
+  this.reset();
+});
